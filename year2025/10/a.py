cc3960398bd2103dcb2725dce7e05aa619514c14
Alojz Milicevic <alojz.milicevic@geoguessr.com>
2025-12-14 08:20:20 +0100

    Day 10 complete i love ilp...

--- year2025/10/a.py
+++ year2025/10/a.py
@@ -1,8 +1,8 @@
 from linereader import read_file
-from helpers import print_table
 from common.parsing import extract_brackets, extract_parens, extract_braces
+from itertools import combinations
 
-data = [x for x in read_file('test.input.txt')]
+data = [x for x in read_file('input.txt')]
 
 def create_matrix(indicator, buttons):
   n = len(indicator)
@@ -38,14 +38,75 @@ def swap_rows(matrix, from_row, to_row):
 def read_line(line):
     indicator = [int(c == '#') for c in extract_brackets(line)]
     buttons = [[int(x) for x in btn.split(',')] for btn in extract_parens(line)]
-    #joltage = [int(x) for x in extract_braces(line).split(',')]
+    joltage = [int(x) for x in extract_braces(line).split(',')]
     
-    return indicator, buttons
+    return indicator, buttons, joltage
 
+def gf2_rref(matrix):
+    n_rows = len(matrix)
+    n_cols = len(matrix[0]) - 1
+    pivot_info = []
+    row = 0
 
-for i in range(1):
-    indicator, buttons = read_line(data[i])
+    for col in range(n_cols):
+        pivot_row = None
+        for r in range(row, n_rows):
+            if matrix[r][col] == 1:
+                pivot_row = r
+                break
+        if pivot_row is None:
+            continue
+
+        if pivot_row != row:
+            swap_rows(matrix, pivot_row, row)
+
+        pivot_info.append((row, col))
+
+        for r in range(row + 1, n_rows):
+            if matrix[r][col] == 1:
+                matrix[r] = combine_rows(matrix[row], matrix[r])
+
+        row += 1
+
+    for pivot_row, pivot_col in reversed(pivot_info):
+        for r in range(pivot_row):
+            if matrix[r][pivot_col] == 1:
+                matrix[r] = combine_rows(matrix[pivot_row], matrix[r])
+
+    return pivot_info
+
+total = 0
+for i in range(len(data)):
+    indicator, buttons, joltage = read_line(data[i])
     matrix = create_matrix(indicator, buttons)
-    print_table(matrix)
-    swap_rows(matrix, 0, 1)
-    print_table(matrix)
+    pivot_info = gf2_rref(matrix)
+    
+    n_cols = len(matrix[0]) - 1
+    pivot_cols = set(col for _, col in pivot_info)
+    free_cols = [col for col in range(n_cols) if col not in pivot_cols]
+    
+    min_presses = None
+    
+    for num_free_pressed in range(len(free_cols) + 1):
+        for pressed_free_cols in combinations(free_cols, num_free_pressed):
+            solution = [0] * n_cols
+            
+            for col in pressed_free_cols:
+                solution[col] = 1
+            
+            for pivot_row, pivot_col in reversed(pivot_info):
+                val = matrix[pivot_row][-1]
+                for col in range(n_cols):
+                    if col != pivot_col:
+                        val ^= (matrix[pivot_row][col] * solution[col])
+                solution[pivot_col] = val
+            
+            presses = sum(solution)
+            if min_presses is None or presses < min_presses:
+                min_presses = presses
+    
+    total += min_presses
+    
+print(f"Total minimum button presses: {total}")
+
+
