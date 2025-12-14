e4b77a31aadc31a8d486e9c1342dcca5ac044f20
Alojz Milicevic <alojz.milicevic@geoguessr.com>
2025-12-14 23:50:55 +0100

    im too sleepy to finish this

--- year2025/11/a.py
+++ year2025/11/a.py
@@ -1,4 +1,28 @@
 from linereader import read_file
 
-data = [x for x in read_file('input.txt')]
+data = [x.split(":") for x in read_file('test.input.txt')]
+
+
+def parse_input(raw_line):
+    root = raw_line[0]
+    rest = raw_line[1]
+
+    rest = rest.strip().split(" ")
+
+    return root, rest
+
+def create_adj_list():
+    d = {}
+
+    for line in data:
+        root, rest = parse_input(line)
+
+        d.setdefault(root, []).extend(rest)
+
+
+    return d
+
+g = create_adj_list()
+
+
 
