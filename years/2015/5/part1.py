f233425527540493a6941717ba44fd1b74f2ef92
Alojz Milicevic <alojz.milicevic@geoguessr.com>
2025-12-15 21:53:13 +0100

    2015-5

--- years/2015/5/part1.py
+++ years/2015/5/part1.py
@@ -1,36 +1,42 @@
 from common.solution import Solution
 import re
 
-vowels_pattern = re.compile("[aeiou]")
-twice_in_a_row_pattern = re.compile(r"(.)\1")
-forbidden_patterns = ["ab", "cd", "pq", "xy"]
+p1_pattern = re.compile(
+    r"^(?!.*(?:ab|cd|pq|xy))"   
+    r"(?=(?:.*[aeiou]){3,})"   
+    r".*(.)\1"                 
+)
 
 class Day(Solution):
-    def has_three_vowels(self, line):
-        return len(vowels_pattern.findall(line)) >= 3
-    
-    def has_double_letter(self, line):
-        return bool(twice_in_a_row_pattern.search(line))
-    
-    def has_no_forbidden_patterns(self, line):
-        for pattern in forbidden_patterns:
-            if pattern in line:
-                return False
-        return True
-    
-    def has_letter_pair_twice(self, line):
-        None
-    
-    def has_repeat_with_one_between(self, line):
-        None
-    
+    def is_nice(self, line):
+        pairs = {}
+        has_pair_twice = False
+        has_repeat_with_gap = False
+
+        for i in range(len(line)):
+            if i + 2 < len(line):
+                if line[i] == line[i + 2]:
+                    has_repeat_with_gap = True
+
+            if i + 1 < len(line):
+                pair = line[i:i + 2]
+
+                if pair not in pairs:
+                    pairs[pair] = i
+                else:
+                    if i >= pairs[pair] + 2:
+                        has_pair_twice = True
+
+            if has_pair_twice and has_repeat_with_gap:
+                return True
+
+        return False
+
     def part1(self):
         count = 0
         
         for line in self.data:
-            if (self.has_three_vowels(line) and
-                self.has_double_letter(line) and
-                self.has_no_forbidden_patterns(line)):
+            if bool(p1_pattern.search(line)):
                 count += 1
         return count
         
@@ -38,11 +44,10 @@ class Day(Solution):
         count = 0
         
         for line in self.data:
-            if(self.has_letter_pair_twice(line) and 
-               self.has_repeat_with_one_between(line)):
+            if(self.is_nice(line)):
                 count +=1
         
         return count
 
 
-Day().solve()
+Day("input.txt").solve()
